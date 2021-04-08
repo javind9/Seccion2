796d1576373e48788cb045a6eca0877b2e4e4a72
Javier <jndominguez@isotrol.com>
2021-04-08 14:54:49 +0200

    17

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 "use strict";
 (function () {
-    function getEdad() {
-        return 100 + 100 + 300;
+    function activar(quien, momento, //? puede ser opcional
+    objeto) {
+        if (objeto === void 0) { objeto = 'batiseñal'; }
+        if (momento) {
+            console.log(quien + " activ\u00F3 la " + objeto + " en la " + momento + ".");
+        }
+        else {
+            console.log(quien + " activ\u00F3 la " + objeto + ".");
+        }
+        // console.log(`${ quien } activó la ${ objeto }`);
     }
-    var nombre = 'Fernando';
-    var apellido = 'Herrera';
-    var edad = 33;
-    //const salida = nombre + apellido + edad;
-    //const salida = nombre + "" + apellido + " ( " + edad + " )";
-    //const salida = `${nombre} \n${apellido} \n( ${edad} )`;
-    var salida = "\n    " + nombre + " \n    " + apellido + " \n    ( " + getEdad() + " )";
-    // Fernando Herrera (Edad: 33)
-    console.log(salida);
+    activar('Gordon', 'tarde');
 })();
