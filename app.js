37cbeca4a9a27003e9d6a2c11d106a6aaf7f9d5f
Javier <jndominguez@isotrol.com>
2021-04-09 13:24:28 +0200

    25

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 "use strict";
 (() => {
-    const enviarMision = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión`);
-    };
-    const regresarAlCuartel = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión`);
-    };
-    const wolverine = {
-        nombre: 'Logan',
-        edad: 60
-    };
-    enviarMision(wolverine);
-    regresarAlCuartel(wolverine);
+    /*
+    class Avenger {
+        nombre: string = 'Sin nombre';
+        equipo: string;
+        nombreReal: string;
+        puedePelear: boolean;
+        peleasGanadas: number;
+
+        constructor( nombre: string, equipo: string, nombreReal: string, puedePelear: boolean, peleasGanadas: number){
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+
+    const antman: Avenger = new Avenger('Antman','Capi','',);
+
+    console.log(antman);
+
+    */
+    class Avenger {
+        /*
+        nombre: string;
+        equipo: string;
+        nombreReal: string;
+        puedePelear: boolean;
+        peleasGanadas: number;
+        */
+        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+    const antman = new Avenger('Antman', 'Capi');
+    console.log(antman);
 })();
