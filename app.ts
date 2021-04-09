ba5891af422748fd9fea86d023cbbcb002d37471
Javier <jndominguez@isotrol.com>
2021-04-09 14:48:12 +0200

    28

--- app.ts
+++ app.ts
@@ -1,47 +1,16 @@
 (() =>{
+    const sumar = (a:number, b:number): number => a + b;
 
-    /*
-    class Avenger {
-        nombre: string = 'Sin nombre';
-        equipo: string;
-        nombreReal: string;
-        puedePelear: boolean;
-        peleasGanadas: number;
+    const  nombre = ():string => 'Hola Fernando';
 
-        constructor( nombre: string, equipo: string, nombreReal: string, puedePelear: boolean, peleasGanadas: number){
-            this.nombre = nombre;
-            this.equipo = equipo;
-            this.nombreReal = nombreReal;
-            this.puedePelear = puedePelear;
-            this.peleasGanadas = peleasGanadas;
-        }
+    const obtenerSalario = ():Promise<string> => {
+        return new Promise( (resolve, reject ) => {
+            resolve('Fernando');
+        });
     }
 
-    const antman: Avenger = new Avenger('Antman','Capi','',);
 
-    console.log(antman);
-
-    */
-
-    class Avenger {
-        /*
-        nombre: string;
-        equipo: string;
-        nombreReal: string;
-        puedePelear: boolean;
-        peleasGanadas: number;
-        */
-        constructor( public nombre:string,
-                     public equipo: string ,
-                     public nombreReal?: string,
-                     public puedePelear: boolean = true,
-                     public peleasGanadas: number = 0){}
-    }
-
-    const antman: Avenger = new Avenger('Antman','Capi');
-
-    console.log(antman);
-    
+    obtenerSalario().then( a => console.log(a.toUpperCase()))
     
 })();
 
