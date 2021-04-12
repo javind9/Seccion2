55d5d3f3c4f63c0a5a6913200b29b9bf27975a98
Javier <jndominguez@isotrol.com>
2021-04-12 09:00:47 +0200

    Examen Practico

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
 "use strict";
 (() => {
-    /*
-    class Avenger {
-        nombre: string = 'Sin nombre';
-        equipo: string;
-        nombreReal: string;
-        puedePelear: boolean;
-        peleasGanadas: number;
-
-        constructor( nombre: string, equipo: string, nombreReal: string, puedePelear: boolean, peleasGanadas: number){
-            this.nombre = nombre;
-            this.equipo = equipo;
-            this.nombreReal = nombreReal;
-            this.puedePelear = puedePelear;
-            this.peleasGanadas = peleasGanadas;
+    // Uso de Let y Const
+    let nombre = 'Ricardo Tapia';
+    let edad = 23;
+    const PERSONAJE = {
+        nombre: nombre,
+        edad: edad
+    };
+    // Convertir esta funcion a una funcion de flecha
+    const resultadoDoble = (a, b) => (a + b) * 2;
+    // Función con parametros obligatorios, opcionales y por defecto
+    // donde NOMBRE = obligatorio
+    //       PODER  = opcional
+    //       ARMA   = por defecto = 'arco'
+    function getAvenger(nombre, poder, arma = 'arco') {
+        let mensaje;
+        if (poder) {
+            mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+        }
+        else {
+            mensaje = nombre + ' tiene un ' + poder;
         }
     }
-
-    const antman: Avenger = new Avenger('Antman','Capi','',);
-
-    console.log(antman);
-
-    */
-    class Avenger {
-        /*
-        nombre: string;
-        equipo: string;
-        nombreReal: string;
-        puedePelear: boolean;
-        peleasGanadas: number;
-        */
-        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
-            this.nombre = nombre;
-            this.equipo = equipo;
-            this.nombreReal = nombreReal;
-            this.puedePelear = puedePelear;
-            this.peleasGanadas = peleasGanadas;
+    ;
+    // Cree una clase que permita manejar la siguiente estructura
+    // La clase se debe de llamar rectangulo,
+    // debe de tener dos propiedades:
+    //   * base
+    //   * altura
+    // También un método que calcule el área  =  base * altura,
+    // ese método debe de retornar un numero.
+    class Rectangulo {
+        constructor(base, altura) {
+            this.base = base;
+            this.altura = altura;
+        }
+        calcularArea() {
+            return this.base * this.altura;
         }
     }
-    const antman = new Avenger('Antman', 'Capi');
-    console.log(antman);
 })();
